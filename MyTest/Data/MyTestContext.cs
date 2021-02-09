66b873d65c789fc4315edbd55c44ad583fe6d598
FabioMattos2707 <fabio.matos2707@outlook.com>
2021-02-08 21:49:09 -0300

    Mytest

---
+++ MyTest/Data/MyTestContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyTest.Models;
+
+namespace MyTest.Data
+{
+    public class MyTestContext : DbContext
+    {
+        public MyTestContext (DbContextOptions<MyTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
---
+++ MyTest/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyTest.Data;
+using MyTest.Models;
+
+namespace MyTest.Services
+{
+    public class SellerService
+    {
+        private readonly MyTestContext _context;
+
+        public SellerService(MyTestContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
