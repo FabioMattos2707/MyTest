f246fd624ae8c32dec3da9ebe202534d4bac3f35
FabioMattos2707 <fabio.matos2707@outlook.com>
2021-02-11 22:49:54 -0300

    Implementing simple search

---
+++ MyTest/Models/ViewModels/SalesRecordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyTest.Models;
+
+namespace MyTest.Models.ViewModels
+{
+    public class SalesRecordViewModel
+    {
+        public Seller Seller { get; set; }
+        public List<SalesRecord> Sales { get; set; }
+    }
+}
---
+++ MyTest/Services/SalesRecordService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyTest.Models;
+using MyTest.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyTest.Services
+{
+    public class SalesRecordService
+    {
+        private readonly MyTestContext _context;
+
+        public SalesRecordService(MyTestContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate);
+            }
+
+            return await result.Include(obj => obj.Seller).Include(obj => obj.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
+        }
+    }
+}
